2f4ed6485fc30ad05c7c294b8e0f4e6e1b94f12f
esoe <igor_nikulin@mail.ru>
2022-12-09 15:25:44 +0300

    qq

--- src/main/java/ru/molokoin/SwingFace.java
+++ src/main/java/ru/molokoin/SwingFace.java
@@ -1,16 +1,41 @@
 package main.java.ru.molokoin;
 
-import javax.swing.JFrame;
 
-public class SwingFace extends JFrame {
+import java.awt.Container;
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class SwingFace extends JPanel {
+    private Container cp;
+    private JPanel NorthPane;
+    private JPanel SouthPane;
+    private JPanel CenterPane;
+    
     public SwingFace(){
+        
         init();
+    }
+    public JFrame initMainframe(){
+        JFrame frame = new JFrame();
+        frame.setBounds(200, 200, 300, 600);
+        frame.setTitle("Калькулятор");
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        return frame;
     }
 
     private void init() {
-        setBounds(200, 200, 300, 600);
-        setTitle("Калькулятор");
-        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        
+        
+        
+        cp = getContentPane();
+        cp.setLayout(new BorderLayout());
         setVisible(true);
+    }
+    public JPanel  initNorthPane(){
+        JPanel pan = new JPanel();
+
+        return pan;
     }
 }
